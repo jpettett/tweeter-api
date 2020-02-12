9fb6886c6fcb19493bf23171d600ecc288cc2234
jpettett <joseph.pettett@gmail.com>
2020-02-12 14:55:36 -0800

    updated crud

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -2,10 +2,30 @@ const express = require('express');
 const router = express.Router();
 const Tweet = require('../models/Tweet');
 
+// Get all Tweets //
 router.get('/', (req, res) => {
-	Tweet.find({}).then(tweets => {
-		res.json(tweets);
-	});
+  Tweet.find({}).then(tweets => {
+    res.json(tweets);
+  });
 });
 
+// POST new Tweet //
+router.post('/post', (req, res) => {
+  const newTweet = req.body;
+  Tweet.create(newTweet).then(gif => res.json('/'));
+});
+
+// PUT edit tweet //
+router.put('/:id', (req, res) => {
+  Tweet.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true
+  }).then(tweet => res.redirect('/'));
+});
+
+// DELETE tweet //
+router.delete('/:id', (req, res) => {
+  Tweet.findOneAndDelete({ _id: req.params.id }).then(tweet =>
+    res.redirect('/')
+  );
+});
 module.exports = router;
